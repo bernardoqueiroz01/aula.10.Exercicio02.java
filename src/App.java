495431ef767d3586cbf72305bd0dba9c591b27e9
bernardo <bernardo.queiroz01@gmail.com>
2026-05-18 16:43:43 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Aluno aluno01 = new Aluno();
+        aluno01.nome = "Maria";
+        aluno01.matricula ="2024001";
+        aluno01.nota01 = 7.5;
+        aluno01.nota02 = 8.0;
+
+        System.out.println(aluno01.exibirBoletim());
+    }
+}
